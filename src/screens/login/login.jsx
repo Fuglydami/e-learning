2c74985889dbfd7cf82ce5677eeed8d7b0600001
Damilare Oyedeji <damoye81@gmail.com>
2023-05-23 20:18:11 +0100

    fixed

--- src/screens/login/login.jsx
+++ src/screens/login/login.jsx
@@ -5,7 +5,7 @@ import { LoginForm } from './loginForm';
 const LoginScreen = () => {
   return (
     <main className='text-mulish'>
-      <div className='flex container  xl:h-screen'>
+      <div className='flex container xl:h-screen'>
         <div className='flex flex-row w-full'>
           <Corousel />
           <LoginForm />
